313ff790242727c076c0a4c03e49c7097af717cc
gaoerxi <gaoerxi@000861.com>
2017-07-03 18:32:18 +0800

    1

--- global.php
+++ global.php
@@ -4,6 +4,7 @@ define('Environmental','.dev');
 if(Environmental=='.dev'){
 	error_reporting(E_ALL & ~E_NOTICE);
 }else{
+    ini_set("display_errors", 0);
 	error_reporting(0);
 }
 define('BASE_ROOT_PATH',str_replace('\\','/',dirname(__FILE__)));
