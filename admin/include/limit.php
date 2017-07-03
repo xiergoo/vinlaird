45998abb6ae81ccc68014fea1253b3a82802d441
gaoerxi <gaoerxi@000861.com>
2017-07-03 12:00:50 +0800

    1

--- admin/include/limit.php
+++ admin/include/limit.php
@@ -8,19 +8,10 @@ defined('InShopNC') or exit('Access Invalid!');
 $_limit =  array(
 	array('name'=>$lang['nc_config'], 'child'=>array(
 		array('name'=>$lang['nc_web_set'], 'op'=>null, 'act'=>'setting'),
-		array('name'=>$lang['nc_web_account_syn'], 'op'=>null, 'act'=>'account'),
-		array('name'=>$lang['nc_upload_set'], 'op'=>null, 'act'=>'upload'),
-		array('name'=>$lang['nc_seo_set'], 'op'=>'seo', 'act'=>'setting'),
-		array('name'=>$lang['nc_pay_method'], 'op'=>null, 'act'=>'payment'),
 		array('name'=>$lang['nc_message_set'], 'op'=>null, 'act'=>'message'),
-		array('name'=>$lang['nc_admin_express_set'], 'op'=>null, 'act'=>'express'),
-		array('name'=>'运单模板', 'op'=>null, 'act'=>'waybill'),
-		//v3-b12
-		array('name'=>$lang['nc_admin_area_manage'], 'op'=>null, 'act'=>'area'),
-		array('name'=>$lang['nc_admin_offpay_area_set'], 'op'=>null, 'act'=>'offpay_area'),
+		array('name'=>$lang['nc_limit_manage'], 'op'=>null, 'act'=>'admin'),
 	    array('name'=>$lang['nc_admin_clear_cache'], 'op'=>null, 'act'=>'cache'),
-	    array('name'=>$lang['nc_admin_perform_opt'], 'op'=>null, 'act'=>'perform'),
-	    array('name'=>$lang['nc_admin_search_set'], 'op'=>null, 'act'=>'search'),
+	    array('name'=>'数据备份', 'op'=>null, 'act'=>'db'),
 	    array('name'=>$lang['nc_admin_log'], 'op'=>null, 'act'=>'admin_log'),
 		)),
 );
