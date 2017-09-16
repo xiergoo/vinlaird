c31d837904101ee2cc7a71aaa0f73c8d85c19e67
gaoerxi <gaoerxi@000861.com>
2017-09-16 21:40:14 +0800

    修改

--- wap/templates/default/index.php
+++ wap/templates/default/index.php
@@ -14,7 +14,7 @@
 </head>
     <?php $peroid = $output['period'] ?>
 <body>
-    <div class="container" id="container">
+    <div class="container" id="container"><!--F43530 E64340 CE3C39-->
         <div style="background-color: #F43530; padding: 2px 0px 2px 15px; font-size: 23px">
             <b>第<?php echo $peroid['pno']; ?>期</b><span style="font-size:18px">（<?php echo date('m月d日H',$peroid['jtime']); ?>点开）</span>
         </div>
@@ -26,11 +26,11 @@
         </div>
         <hr style="margin: 13px 0px 1px 0px" />
         <div style="padding-left: 13px;">
-            <button class="weui-btn weui-btn_mini weui-btn_plain-primary">100</button>
-            <button class="weui-btn weui-btn_mini weui-btn_plain-primary weui-btn_plain-disabled">1000</button>
-            <button class="weui-btn weui-btn_mini weui-btn_plain-primary">2000</button>
-            <button class="weui-btn weui-btn_mini weui-btn_plain-primary">5000</button>
-            <button class="weui-btn weui-btn_mini weui-btn_plain-primary">10000</button>
+            <button class="weui-btn weui-btn_mini weui-btn_plain-primary btn_score_times" >100</button>
+            <button class="weui-btn weui-btn_mini weui-btn_plain-primary btn_score_times weui-btn_plain-disabled" >1000</button>
+            <button class="weui-btn weui-btn_mini weui-btn_plain-primary btn_score_times" >2000</button>
+            <button class="weui-btn weui-btn_mini weui-btn_plain-primary btn_score_times" >5000</button>
+            <button class="weui-btn weui-btn_mini weui-btn_plain-primary btn_score_times" >10000</button>
         </div>
         <hr style="margin: 13px 0px 13px 0px" />
         <div style="padding-left: 13px;">
@@ -38,7 +38,7 @@
         </div>
         <hr style="margin: 13px 0px 13px 0px" />
         <div>
-            <button class="weui-btn weui-btn_primary">确定</button>
+            <button class="weui-btn weui-btn_primary weui-btn_disabled btn_submit">确定</button>
         </div>
     </div>
 </body>
@@ -57,9 +57,18 @@
             }
             btn.addClass('weui-btn_plain-disabled');
             $("#span_selected").append('<button style="margin:3px;" class="weui-btn weui-btn_mini weui-btn_plain-default" onclick="cancel_selected(this)" data-num="' + btn.attr('data-num') + '">' + btn.attr('data-num') + '</button>');
-
+            calc_socre();
         })
 
+        $(".btn_score_times").click(function () {
+            if ($(this).hasClass('weui-btn_plain-disabled')) {
+                return false;
+            }
+            $(".btn_score_times").removeClass('weui-btn_plain-disabled');
+            $(this).addClass('weui-btn_plain-disabled');
+            times = parseInt($(this).text());
+            calc_socre();
+        })
     })
     function cancel_selected(obj) {
         var btn = $(obj);
@@ -67,6 +76,18 @@
         btn_num.attr('data-hide', 0);
         btn_num.removeClass('weui-btn_plain-disabled');
         btn.remove();
+        calc_socre();
     }
+    var times = 1000;
+    function calc_socre() {
+        var score = $("#span_selected").children().length * times;
+        $("#span_socre").html(score);
+        if (score > 0) {
+            $(".btn_submit").removeClass('weui-btn_disabled');
+        } else {
+            $(".btn_submit").addClass('weui-btn_disabled');
+        }
+    }
+    calc_socre();
 </script>
 </html>
