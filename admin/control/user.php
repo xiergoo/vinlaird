d7fdeff7794e8cf1e12476c84d77d9d008b0340a
gaoerxi <gaoerxi@000861.com>
2017-09-18 09:38:27 +0800

    修改

---
+++ admin/control/user.php
@@ -0,0 +1,20 @@
+<?php
+
+defined('InShopNC') or exit('Access Invalid!');
+
+class userControl extends SystemControl{
+	public function __construct(){
+		parent::__construct();
+		Language::read('index');
+	}
+	public function indexOp(){
+		$model_user = Model('user');
+        $list = $model_user->order('id desc')->page(20)->select();        
+        $page=pagecmd('obj');
+        Tpl::output('_page',$page->show());
+	}
+    
+    public function rechargeOp(){
+        
+    }
+}
