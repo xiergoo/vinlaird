15b554535f9be3d3d0ca5ca8392b3455125927ef
gaoerxi <gaoerxi@000861.com>
2017-12-27 18:18:34 +0800

    1

--- admin/control/period.php
+++ admin/control/period.php
@@ -17,4 +17,59 @@ class periodControl extends SystemControl{
         $page=pagecmd('obj');
         Tpl::output('_page',$page->show());
 	}
+    
+    public function openPrizeOp(){
+        if(IS_POST){
+            $periodID=intval($_POST['id']);
+            $periodInfo=periodClass::I()->getOne($periodID);
+            $type = typeClass::I()->getOne($periodInfo['type_id'],false);
+            if($periodInfo['pstatus']==periodClass::status_online){
+                $dpnum=floatval($_POST['dpnum']);
+                $jnum=$dpnum*100%$type['mod'];
+                Model::beginTransaction();
+                $result = Model('order')->where(['pid'=>$periodInfo['id'],'num'=>$jnum])->update(['is_right'=>1,'prize_score'=>['exp',$type['times'].'*score']]);
+                if($result){
+                    $inScore=Model('order')->where(['pid'=>$periodInfo['id']])->sum('score');
+                    $outScore=Model('order')->where(['pid'=>$periodInfo['id'],'is_right'=>1])->sum('prize_score');
+                    $data['dpnum']=$dpnum;
+                    $data['jnum']=$jnum;
+                    $data['inscore']=$inScore;
+                    $data['outscore']=$outScore;
+                    $data['pstatus']=periodClass::status_wait;
+                    $result = periodClass::I()->where(['id'=>$period_info['id']])->update($data);
+                    if($result){
+                        Model::commit();
+                        showMessage('³É¹¦');
+                    }else{
+                        Model::rollback();
+                        showMessage('Ê§°Ü');
+                    }
+                }else{
+                    Model::rollback();
+                    showMessage('Ê§°Ü');
+                }                
+            }elseif($periodInfo['pstatus']==periodClass::status_wait){
+                Model::beginTransaction();
+                $result = Model('order')->where(['pid'=>$periodInfo['id'],'num'=>$periodInfo['jnum']])->update(['is_right'=>1,'prize_score'=>['exp',$type['times'].'*score']]);
+                if($result){
+                    $inScore=Model('order')->where(['pid'=>$periodInfo['id']])->sum('score');
+                    $outScore=Model('order')->where(['pid'=>$periodInfo['id'],'is_right'=>1])->sum('prize_score');                    
+                    $data['inscore']=$inScore;
+                    $data['outscore']=$outScore;
+                    $result = periodClass::I()->where(['id'=>$period_info['id']])->update($data);
+                    if($result){
+                        Model::commit();
+                        showMessage('³É¹¦');
+                    }else{
+                        Model::rollback();
+                        showMessage('Ê§°Ü');
+                    }
+                }else{
+                    Model::rollback();
+                    showMessage('Ê§°Ü');
+                } 
+            }
+        }else{
+        }
+    }
 }
--- cron/control/index.php
+++ cron/control/index.php
@@ -22,13 +22,28 @@ class indexControl extends BaseControl{
         {
         	$periodInfo = periodClass::I()->where(['type_id'=>$type['id'],'pstatus'=>periodClass::status_online,'jtime'=>['eq',dapanClass::beforeTime()]])->find();
             if($periodInfo['id']>0){
-                $data['dpnum']=dapanClass::value();
-                if($data['dpnum']>1000){
-                    $data['jnum']=$data['dpnum']*100%$type['mod'];
-                    $data['pstatus']=periodClass::status_wait;
-                    $result = periodClass::I()->where(['id'=>$period_info['id']])->update($data);
+                
+                $dpnum=dapanClass::value();
+                if($dpnum>1000){
+                    $jnum=$dpnum*100%$type['mod'];
+                    Model::beginTransaction();
+                    $result = Model('order')->where(['pid'=>$periodInfo['id'],'num'=>$jnum])->update(['is_right'=>1,'prize_score'=>['exp',$type['times'].'*score']]);
                     if($result){
-                        Model('order')->where(['pid'=>$periodInfo['id'],'num'=>$data['jnum']])->update(['is_right'=>1,'prize_score'=>['exp',$type['times'].'*score']]);
+                        $inScore=Model('order')->where(['pid'=>$periodInfo['id']])->sum('score');
+                        $outScore=Model('order')->where(['pid'=>$periodInfo['id'],'is_right'=>1])->sum('prize_score');
+                        $data['dpnum']=$dpnum;
+                        $data['jnum']=$jnum;
+                        $data['inscore']=$inScore;
+                        $data['outscore']=$outScore;
+                        $data['pstatus']=periodClass::status_wait;
+                        $result = periodClass::I()->where(['id'=>$period_info['id']])->update($data);
+                        if($result){
+                            Model::commit();
+                        }else{
+                            Model::rollback();
+                        }
+                    }else{
+                        Model::rollback();
                     }
                 }
             }
