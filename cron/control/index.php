b42b665ec8a2c96cce81f2df68e9234a6eadaa78
gaoerxi <gaoerxi@000861.com>
2017-12-27 16:28:03 +0800

    1

--- cron/control/index.php
+++ cron/control/index.php
@@ -17,17 +17,19 @@ class indexControl extends BaseControl{
     }
     
     public function openPrize(){
-        $listType = typeClass::I()->lists(['enable'=>typeClass::status_enable]);
+        $listType = typeClass::I()->lists();
         foreach ($listType as $type)
         {
         	$periodInfo = periodClass::I()->where(['type_id'=>$type['id'],'pstatus'=>periodClass::status_online,'jtime'=>['eq',dapanClass::beforeTime()]])->find();
-            $data['dpnum']=dapanClass::value();
-            if($data['dpnum']>1000){
-                $data['jnum']=$data['dpnum']*100%$type['mod'];
-                $data['pstatus']=periodClass::status_wait;
-                $result = periodClass::I()->where(['id'=>$period_info['id']])->update($data);
-                if($result){
-                    Model('order')->where(['pid'=>$periodInfo['id'],'num'=>$data['jnum']])->update(['is_right'=>1]);
+            if($periodInfo['id']>0){
+                $data['dpnum']=dapanClass::value();
+                if($data['dpnum']>1000){
+                    $data['jnum']=$data['dpnum']*100%$type['mod'];
+                    $data['pstatus']=periodClass::status_wait;
+                    $result = periodClass::I()->where(['id'=>$period_info['id']])->update($data);
+                    if($result){
+                        Model('order')->where(['pid'=>$periodInfo['id'],'num'=>$data['jnum']])->update(['is_right'=>1]);
+                    }
                 }
             }
         }        
